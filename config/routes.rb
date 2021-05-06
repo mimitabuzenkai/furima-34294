2643239e4a6e2329afe7e8b99aa588a9645aaa0f
mimitabuzenkai <77916164+mimitabuzenkai@users.noreply.github.com>
2021-05-06 20:48:51 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :tweets, only: :index
+  # root to: 'prototypes#index'
+
+  root to: 'items#index'
 end
+
+
+# 先にコントローラーを実装 items
+# modelは後で消す必要があるかもしれない。
+# 再度、マイグレート修正する必要あり？
