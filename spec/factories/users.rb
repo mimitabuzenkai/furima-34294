42e94d49a5c352ba69774d7aff9c9c5d03505913
mimitabuzenkai <77916164+mimitabuzenkai@users.noreply.github.com>
2021-05-11 20:35:11 +0900

    ユーザー管理機能の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
+
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     first_name           {"大久保"}
     first_name_kana      {"オオクボ"}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   before do
     @user = FactoryBot.build(:user)
   end
@@ -19,7 +18,7 @@ RSpec.describe User, type: :model do
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
     
     it 'メールアドレスは、@を含む必要があること' do
@@ -41,11 +40,59 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
+    it 'パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
     it 'パスワードとパスワード（確認用）、値の一致が必須であること' do
       @user.password = '123456'
       @user.password_confirmation = '1234567'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
+      @user.password = "000aaa"
+      @user.password_confirmation = '000000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
+      @user.password = "000aaa"
+      @user.password_confirmation = '000000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+      @user.first_name = "AAA"
+      @user.last_name = "AAA"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid", "Last name is invalid")
+    end
+
+    it 'ユーザー本名のフリガナは、名字と名前がそれぞれ必須であること' do
+      @user.first_name_kana = ""
+      @user.last_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana is invalid", "Last name kana is invalid")
+    end
+
+    it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
+      @user.first_name_kana = "あA"
+      @user.last_name_kana = "あA"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana is invalid", "Last name kana is invalid")
+    end
+
+    it '生年月日が必須であること' do
+      @user.birth = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth can't be blank")
     end
   end
 end
