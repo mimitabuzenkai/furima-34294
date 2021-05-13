b995009765b25de9fe7d35c6e5f6ddd05136d539
mimitabuzenkai <77916164+mimitabuzenkai@users.noreply.github.com>
2021-05-13 20:40:47 +0900

    ユーザー管理機能修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
        
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   with_options presence: true, format: { with: PASSWORD_REGEX, message:'には英字と数字の両方を含めて設定してください' }, length: { minimum: 6 }, confirmation: true do
@@ -11,12 +9,11 @@ class User < ApplicationRecord
   end
 
   with_options presence: true do
+    validates :nickname, presence: true
     validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-    validates_format_of :last_name, with: /\A[ぁ-んァ-ン一-龥]+\z/
+    validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
     validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/
     validates_format_of :last_name_kana, with: /\A[ァ-ヶー－]+\z/
+    validates :birth, presence: true
   end
-
-  validates :birth, presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,15 @@ FactoryBot.define do
   factory :user do
 
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
+    # email                 {Faker::Internet.free_email}
+    email                 {"hoge@example.com"}
+
     password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
-    first_name           {"大久保"}
-    first_name_kana      {"オオクボ"}
-    last_name            {"精剛"}
-    last_name_kana       {"セイゴウ"}
+    first_name           {"中村"}
+    first_name_kana      {"ナカムラ"}
+    last_name            {"勇気"}
+    last_name_kana       {"ユウキ"}
     birth                {"2000-01-01"}
   end
 end
