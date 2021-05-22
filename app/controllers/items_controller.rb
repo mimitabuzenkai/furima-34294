9d690776508e73ce258a5145e54a45995adf3fcd
mimitabuzenkai <77916164+mimitabuzenkai@users.noreply.github.com>
2021-05-22 20:41:08 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :new, :show]
 
