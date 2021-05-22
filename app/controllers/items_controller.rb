34c717822ff6ae37fba9f6d5367d9d386a6778f9
mimitabuzenkai <77916164+mimitabuzenkai@users.noreply.github.com>
2021-05-22 22:27:21 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :new, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
